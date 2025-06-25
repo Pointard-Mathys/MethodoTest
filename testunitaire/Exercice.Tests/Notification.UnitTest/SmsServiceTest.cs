6b6bb03a9a22358345c05d2df667927d6385dc06
Princesse Patate <lea.regoudis@gmail.com>
2025-06-25 12:47:54 +0200

    tests exo 5

---
+++ testunitaire/Exercice.Tests/Notification.UnitTest/SmsServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using FluentAssertions;
+using Notification.Contracts;
+using Notification.Services;
+
+namespace Notification.UnitTest;
+
+public class SmsServiceTest
+{
+    [Fact]
+    public void IsValidPhoneNumber_ShouldValidateCorrectly()
+    {
+        var service = new SmsService();
+
+        service.IsValidPhoneNumber("+33612345678").Should().BeTrue();
+        service.IsValidPhoneNumber("badphone").Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task SendSmsAsync_ShouldReturnTrue()
+    {
+        var service = new SmsService();
+        var result = await service.SendSmsAsync("+33612345678", "Hello");
+
+        result.Should().BeTrue();
+    }
+}
+
